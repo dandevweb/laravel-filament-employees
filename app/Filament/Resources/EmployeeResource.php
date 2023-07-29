0d0123a0a1388cc85e1eb5274cccd7df1a2c0641
dandevweb <dandevweb@gmail.com>
2023-07-29 09:01:08 -0300

    feat: selects dependant of relationship

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -2,16 +2,24 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\EmployeeResource\Pages;
-use App\Filament\Resources\EmployeeResource\RelationManagers;
-use App\Models\Employee;
 use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
 use Filament\Tables;
+use App\Models\State;
+use App\Models\Country;
+use App\Models\Employee;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\EmployeeResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
 
 class EmployeeResource extends Resource
 {
@@ -23,7 +31,43 @@ class EmployeeResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make()
+                    ->schema([
+                        Select::make('country_id')
+                            ->label('Country')
+                            ->options(Country::all()->pluck('name', 'id')->toArray())
+                            ->reactive(),
+
+                        Select::make('state_id')
+                            ->label('State')
+                            ->options(function (callable $get) {
+                                $country = Country::find($get('country_id'));
+                                return $country ? $country->states->pluck('name', 'id')->toArray() : [];
+                            })
+                            ->reactive()
+                            ->disabled(function (callable $get) {
+                                return !$get('country_id');
+                            }),
+
+                        Select::make('city_id')
+                            ->label('City')
+                            ->options(function (callable $get) {
+                                $state = State::find($get('state_id'));
+                                return $state ? $state->cities->pluck('name', 'id')->toArray() : [];
+                            })
+                            ->reactive()
+                            ->disabled(function (callable $get) {
+                                return !$get('state_id');
+                            }),
+
+                        Select::make('department_id')->relationship('department', 'name')->required(),
+                        TextInput::make('first_name')->required(),
+                        TextInput::make('last_name')->required(),
+                        TextInput::make('address')->required(),
+                        TextInput::make('zip_code')->required(),
+                        DatePicker::make('birth_date')->required(),
+                        DatePicker::make('date_hired')->required(),
+                    ])
             ]);
     }
 
@@ -31,10 +75,14 @@ class EmployeeResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('first_name')->searchable()->sortable(),
+                TextColumn::make('last_name')->searchable()->sortable(),
+                TextColumn::make('department.name')->sortable(),
+                TextColumn::make('created_at')->sortable(),
             ])
             ->filters([
-                //
+                SelectFilter::make('department_id')->relationship('department', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -43,14 +91,14 @@ class EmployeeResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -58,5 +106,5 @@ class EmployeeResource extends Resource
             'create' => Pages\CreateEmployee::route('/create'),
             'edit' => Pages\EditEmployee::route('/{record}/edit'),
         ];
-    }    
+    }
 }
